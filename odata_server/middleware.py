5f539668c00f9934d6d868bd932e9eb123422d22
Álvaro Arranz <aarranz@ficodes.com>
2021-12-22 02:51:52 +0100

    Update HTTPMethodOverrideMiddleware so method does not use bytes

--- odata_server/middleware.py
+++ odata_server/middleware.py
@@ -23,7 +23,6 @@ class HTTPMethodOverrideMiddleware(object):
     def __call__(self, environ, start_response):
         method = environ.get("HTTP_X_HTTP_METHOD", "").upper()
         if method in self.allowed_methods:
-            method = method.encode("ascii", "replace")
             environ["REQUEST_METHOD"] = method
         if method in self.bodyless_methods:
             environ["CONTENT_LENGTH"] = "0"
